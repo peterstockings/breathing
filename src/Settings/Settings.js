baa51bfa1014f8605a27a00f5e700a801662d19a
Peter Stockings <pstockings@jadeworld.com>
2020-12-30 17:38:22 +1100

    Finish implementation for editing breathing stages

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -7,6 +7,15 @@ import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reac
 
 let getLast = (arr = null, n = null) => { if (arr == null) return void 0; if (n === null) return arr[arr.length - 1]; return arr.slice(Math.max(arr.length - n, 0)); };
 
+const getCurrentPosition = (states) => {
+    let mappings = {
+        'In': 1,
+        'Hold': 0,
+        'Out': -1,
+    }
+    return states.reduce((a, c) => a + mappings[c.name], 0)
+}
+
 function LabelAndSlider(props) {
     return (
         <p>
@@ -21,38 +30,40 @@ function LabelAndSlider(props) {
 
 function BreathingStageDropDown(props) {
     let last = getLast(props.states)
-    if (last == undefined)
+
+    let breathingPos = getCurrentPosition(props.states)
+
+    if (last === undefined)
         return (
             <DropdownMenu>
                 <DropdownItem onClick={e => props.addStage('In')}>In</DropdownItem>
                 <DropdownItem disabled onClick={e => props.addStage('Out')}>Out</DropdownItem>
-                <DropdownItem disabled onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
+                <DropdownItem onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
             </DropdownMenu>)
 
-    if (last.name === 'In')
+    if (breathingPos === 1)
         return (
             <DropdownMenu>
                 <DropdownItem disabled onClick={e => props.addStage('In')}>In</DropdownItem>
                 <DropdownItem onClick={e => props.addStage('Out')}>Out</DropdownItem>
-                <DropdownItem onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
+                <DropdownItem disabled={last.name === 'Hold'} onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
             </DropdownMenu>
         )
 
-    if (last.name === 'Out')
+    if (breathingPos === 0)
         return (
             <DropdownMenu>
                 <DropdownItem onClick={e => props.addStage('In')}>In</DropdownItem>
                 <DropdownItem disabled onClick={e => props.addStage('Out')}>Out</DropdownItem>
-                <DropdownItem onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
+                <DropdownItem disabled={last.name === 'Hold'} onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
             </DropdownMenu>
         )
-
 
     return (
         <DropdownMenu>
             <DropdownItem onClick={e => props.addStage('In')}>In</DropdownItem>
             <DropdownItem onClick={e => props.addStage('Out')}>Out</DropdownItem>
-            <DropdownItem onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
+            <DropdownItem disabled onClick={e => props.addStage('Hold')}>Hold</DropdownItem>
         </DropdownMenu>
     )
 }
@@ -75,12 +86,24 @@ function Settings(props) {
 
     const addStage = (name) => {
         let last = getLast(states);
-        console.log('states', states)
-        console.log('name', name)
-        console.log('last', last)
+        let breathingPos = getCurrentPosition(states)
+
+        const getTimerFn = (name, pos) => {
+            if (name === 'In')
+                return t => t
+            if (name === 'Out')
+                return t => (1 - t) < 0.01 ? 0.01 : 1 - t
+            if (name === 'Hold') {
+                if (pos === 0)
+                    return t => 0.01
+                if (pos === 1)
+                    return t => 1
+            }
+        }
+
         setStates(states.concat({
             name,
-            f: t => t,
+            f: getTimerFn(name, breathingPos),
             duration: 1,
             idx: states.reduce((a, c) => c.idx > a ? c.idx : a, 0) + 1,
             id: uuid()
@@ -106,7 +129,7 @@ function Settings(props) {
                 </DropdownToggle>
                 <BreathingStageDropDown states={states} addStage={addStage} />
             </ButtonDropdown>
-            <button disabled={states.length === 0} className="button is-dark" onClick={handleClick}>Start</button>
+            <button disabled={states.length === 0 || getCurrentPosition(states) !== 0} className="button is-dark" onClick={handleClick}>Start</button>
 
         </div>);
 }
