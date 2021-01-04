42eba6cd917f406a85e8fd188e3f76641d3cbdfa
Peter Stockings <pstockings@jadeworld.com>
2021-01-04 14:47:52 +1100

    Fix issue where JSON.stringify doesnt preserve methods

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,10 +1,30 @@
+var JSONfn;
+if (!JSONfn) {
+    JSONfn = {};
+}
+
+(function () {
+    JSONfn.stringify = function (obj) {
+        return JSON.stringify(obj, function (key, value) {
+            return (typeof value === 'function') ? value.toString() : value;
+        });
+    }
+
+    JSONfn.parse = function (str) {
+        return JSON.parse(str, function (key, value) {
+            if (typeof value != 'string') return value;
+            return (value.substring(0, 8) === 'function') ? eval('(' + value + ')') : value;
+        });
+    }
+}());
+
 export const loadState = () => {
     try {
         const serializedState = localStorage.getItem('state');
         if (serializedState === null) {
             return undefined;
         }
-        return JSON.parse(serializedState);
+        return JSONfn.parse(serializedState);
     } catch (err) {
         return undefined;
     }
@@ -12,7 +32,7 @@ export const loadState = () => {
 
 export const saveState = (state) => {
     try {
-        const serializedState = JSON.stringify(state);
+        const serializedState = JSONfn.stringify(state);
         localStorage.setItem('state', serializedState);
     } catch (err) {
         // Ignore write errors.
