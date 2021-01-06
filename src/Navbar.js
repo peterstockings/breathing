64cf9fcbe2dd06a0aed11ada2da4876f32536737
Peter Stockings <pstockings@jadeworld.com>
2021-01-06 16:14:56 +1100

    Changed icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="/">
-                    <img src="https://img.icons8.com/ios/452/breath.png" alt="" />
+                    <img src="/breath.png" alt="" />
                 </a>
             </div>
             <div className="navbar-menu">
